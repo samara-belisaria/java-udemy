574cea3e9769dfa8e9d4625eaab6798a82f7b493
Samara Belisária <samarabelisaria18@gmail.com>
2025-10-06 01:11:44 -0300

    Adiciona projeto Diferença de Produtos
    Inclui o código-fonte e o arquivo README.md do exercício "Diferença de Produtos". 
    O programa lê quatro valores inteiros e calcula a diferença entre o produto de A e B e o produto de C e D.
    Objetivo: praticar operadores aritméticos e entrada de dados com Scanner em Java.

---
+++ diferenca-de-produtos/Diferenca.java
@@ -0,0 +1,25 @@
+package br.com.udemy.lesson;
+
+import java.util.Scanner;
+
+public class Diferenca {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int A, B, C, D, diferenca; 
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		diferenca = A * B - C * D;
+		
+		System.out.println("DIFERENÇA = " + diferenca);
+
+		sc.close();
+	}
+
+}
